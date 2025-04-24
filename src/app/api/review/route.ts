19fc9b1a1e9c8d6417cb8cb24aa05cb26f1e4ebd
joseph-branch <46014002+joseph-branch@users.noreply.github.com>
2025-04-24 12:24:37 -0500

    Update route.ts

--- src/app/api/review/route.ts
+++ src/app/api/review/route.ts
@@ -15,8 +15,6 @@ const openai = createOpenAI({
   compatibility: "strict",
 });
 
-console.log(process.env.GITHUB_SECRET)
-
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN! });
 
 export async function POST(request: NextRequest) {
