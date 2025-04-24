578e97c00dd42790cdda975290b63f2489bc7335
joseph-branch <brnch_jsph@yahoo.com>
2025-04-24 01:21:51 -0500

    Test Init

--- src/app/api/review/route.ts
+++ src/app/api/review/route.ts
@@ -1,8 +1,16 @@
 import { Octokit } from "@octokit/rest";
+import { NextRequest } from "next/server";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-/* eslint-disable @typescript-eslint/no-unused-vars */
 
-export function GET(request: Request) {
-  return new Response("Hello from Vercel!");
+export async function POST(request: NextRequest) {
+  const { owner, repo, pr } = await request.json();
+
+  const { data: reviews } = await octokit.rest.pulls.listReviews({
+    owner,
+    repo,
+    pull_number: parseInt(pr),
+  });
+
+  return new Response(JSON.stringify(reviews));
 }
